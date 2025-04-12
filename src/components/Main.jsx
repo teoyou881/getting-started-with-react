69ffebb40eb1696da3884c2c393ea31181409bb2
Teo_You <teoyou881@gmail.com>
2025-04-12 13:26:37 -0400

    introduce jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Main.css';
+/*
+  * Only JavaScript expressions are allowed inside curly braces ({})
+    → It must be something that evaluates to a value.
 
+    Only numbers, strings, and arrays can be rendered to the screen
+    → boolean, undefined, null, and objects won’t be displayed.
+
+    All tags must be properly closed
+    → Self-closing tags like <img />, <input /> must include the slash.
+
+    JSX must have a single top-level tag
+    → Wrap multiple elements in a parent element like a <div> or <>.
+* */
 export default function Main() {
+  const user = {
+    name: 'test',
+    isLogin: true
+  };
+
   return (
-      <main>
-        <h1>main</h1>
-      </main>
+      <>
+        {user.isLogin ? (
+            <div className={"logout"}>logout</div>
+        ) : (
+            <div>login</div>
+        )}
+      </>
   );
 }
